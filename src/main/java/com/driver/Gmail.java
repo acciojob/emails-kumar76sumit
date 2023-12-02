6bcd063070fe559e978c6b11e4a2cf87df6d89a5
Sumit <kumar76sumit@gmail.com>
2023-12-03 00:51:25 +0530

    check gmail

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -48,7 +48,7 @@ public class Gmail extends Email {
         // It is guaranteed that:
         // 1. Each mail in the inbox is distinct.
         // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
-        if(inbox.size()==inboxCapacity)
+        if(inbox.size()>=inboxCapacity)
         {
             Mail oldMail=inbox.removeFirst();
             trash.addLast(oldMail);
