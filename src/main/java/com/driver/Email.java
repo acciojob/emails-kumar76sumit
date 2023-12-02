864aceee4c48ddc7e3537dd27f511c6a5e6e2627
Sumit <kumar76sumit@gmail.com>
2023-12-02 23:47:46 +0530

    except findMaxMeet

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -10,6 +10,14 @@ public class Email {
         this.password = "Accio@123";
     }
 
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getEmailId() {
         return emailId;
     }
@@ -25,5 +33,40 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if(oldPassword.equals(getPassword()))
+        {
+            boolean checkForUpperCase=false;
+            boolean checkForLowerCase=false;
+            boolean checkForDigit=false;
+            boolean checkForSpecialChar=false;
+            if(newPassword.length()==8)
+            {
+                for(int i=0;i<8;i++)
+                {
+                    char ch=newPassword.charAt(i);
+                    if(ch>='a' && ch<='z')
+                    {
+                        checkForLowerCase=true;
+                    }
+                    if(ch>='A' && ch<='Z')
+                    {
+                        checkForUpperCase=true;
+                    }
+                    if(ch>='0' && ch<='9')
+                    {
+                        checkForDigit=true;
+                    }
+                    else
+                    {
+                        checkForSpecialChar=true;
+                    }
+                }
+            }
+            if(checkForUpperCase && checkForLowerCase && checkForDigit && checkForSpecialChar)
+            {
+                setPassword(newPassword);
+            }
+        }
     }
 }
